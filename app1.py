741369f80dce9d6634bcce3188bc7e4c5e112bee
Samyak Shambharkar <samyakshambharkar05@gmail.com>
2026-06-13 15:49:49 +0530

    Remove unnecessary pip install command
    Removed pip install command for streamlit.

--- app1.py
+++ app1.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/12GFIAvLrZ7pZUGfLVGlMUtlrtxIFvEVC
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import pickle
